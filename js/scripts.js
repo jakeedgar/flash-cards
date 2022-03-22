881861c6eab2f0bc9c8c4aefb3ce189206a1de3f
Jake Edgar <jakeedgar1012@gmail.com>
2022-03-22 10:01:40 -0700

    initial commit
    co-authored-by: Timothy Gibson <ryaninlux@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+
+  $("button#primary").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+
+  $("button#success").click(function() {
+    $("body").removeClass();
+
+  });
+  
+}); 
